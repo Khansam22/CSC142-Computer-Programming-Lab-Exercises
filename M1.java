307d497c64cc89524133be3c52f7038a90c96a6b
Khansam22 <Seahawksameer@hotmail.com>
2025-07-17 00:43:32 -0700

    Add files via upload

---
+++ M1.java
@@ -0,0 +1,8 @@
+public class M1{
+	
+	public static void main(String[] args) {
+		System.out.println("Couse Name: CSC142 OA1 9956 - SU25 - Computer Programming I");
+		System.out.println("I learned in Module 1 that Java programs require a main method ");
+		System.out.println("and Java has strict syntax rules such as the use of braces {}, semicolons ;, and proper indentation.");
+	}
+}
